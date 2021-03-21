aba58ba7b9b6ffa4a4bfba925e60f8decaa55f28
glennbech <glenn.bech@gmail.com>
2021-03-21 14:26:31 +0100

    service account gone

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "google_service_account" "default" {
-  account_id   = "travis-deployer"
-  display_name = "Service Account"
-}
-
 resource "google_compute_instance" "default" {
   name         = "account"
   machine_type = var.machinetype
