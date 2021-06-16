7a4815a926a1a11c27448a2d21fbca8d0d54944e
glennbech <glenn.bech@gmail.com>
2021-06-16 11:12:10 +0200

    test for klagesensur

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "google_service_account" "default" {
+  account_id   = "travis-deployer"
+  display_name = "Service Account"
+}
+
 resource "google_compute_instance" "default" {
   name         = "account"
   machine_type = var.machinetype
